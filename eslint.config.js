74cd581a4ae8b27561b2ff28f23b5034e0eb0070
Abhinav Neelam <abhinavneelam949@gmail.com>
2024-02-11 10:48:50 -0800

    using typescript as additional linter to check for types

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,57 @@
+import js from "@eslint/js";
 import eslint_prettier from "eslint-config-prettier";
 import tsplugin from "@typescript-eslint/eslint-plugin";
 import tsparser from "@typescript-eslint/parser";
+import globals from "globals";
+
+import { FlatCompat } from "@eslint/eslintrc";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({ resolvePluginsRelativeTo: __dirname });
 
 export default [
+    ...compat.config({
+        overrides: [
+            {
+                files: ["src/**/*.ts"]
+            }
+        ],
+        extends: ["plugin:@typescript-eslint/recommended", "plugin:@typescript-eslint/recommended-type-checked"],
+        plugins: ["@typescript-eslint"],
+        parser: "@typescript-eslint/parser",
+        parserOptions: {
+            project: true,
+            tsconfigRootDir: __dirname
+        },
+        ignorePatterns: ["**/**.js"],
+        root: true
+    }),
     {
-        files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
+        files: ["src/*.{mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
+        ignores: ["**/*.js", "node_modules/*"],
         languageOptions: {
             parser: tsparser,
             parserOptions: {
                 sourceType: "module",
                 ecmaFeatures: { modules: true },
                 ecmaVersion: "latest"
+            },
+            globals: {
+                ...globals.node
             }
         },
         plugins: {
             eslint_prettier,
-            ts: tsplugin
+            tsplugin
         },
         rules: {
-            ...tsplugin.configs["eslint-recommended"].rules
+            ...js.configs.recommended.rules,
+            ...eslint_prettier.rules
         }
     },
     eslint_prettier
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 console.log(3);
 let f: number = "";
+f = 3;
 console.log(f);
