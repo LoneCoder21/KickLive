1e747be58c873059b8599a214465ddd7eb912fb0
Abhinav Neelam <abhinavneelam949@gmail.com>
2024-02-14 11:10:34 -0800

    using actionrow with link buttons instead of multiple buttons

--- src/commands/utility/list.ts
+++ src/commands/utility/list.ts
@@ -1,43 +1,36 @@
 import {
     ButtonStyle,
     ButtonBuilder,
-    EmbedBuilder,
     SlashCommandBuilder,
     ChatInputCommandInteraction,
     ActionRowBuilder,
     ChatInputCommandInteraction,
     SlashCommandBuilder
 } from "discord.js";
-import { User } from "../../types/User.js";
-import { headers } from "../../constants/headers.js";
-import axios from "axios";
 
-const list = ["xqc", "destiny", "roshtein", "gmhikaru", "pgl"];
+const list = ["xqc", "destiny", "roshtein", "gmhikaru", "pgl", "sliker"];
 
 export async function execute(interaction: ChatInputCommandInteraction) {
-    const embeds = [];
-
+    const buttons = [];
+    list = list.slice(0, 25);
     for (const streamer of list) {
-        const res = await axios.get(`https://kick.com/api/v2/channels/${streamer}`, {
-            headers: headers
-        });
-        const user: User = res.data.user;
-        console.log(user);
-        const embed = new EmbedBuilder()
-            .setColor(0x00ff7f)
-            .setAuthor({ name: streamer, iconURL: user.profile_pic, url: `https://kick.com/${streamer}` });
-
-        embeds.push(embed);
+        const button = new ButtonBuilder()
+            .setLabel(streamer)
+            .setURL(`https://kick.com/${streamer}`)
+            .setStyle(ButtonStyle.Link);
+        buttons.push(button);
     }
 
-    const github = new ButtonBuilder()
-        .setLabel("GitHub")
-        .setURL("https://github.com/LoneCoder21/KickLive")
-        .setStyle(ButtonStyle.Link);
+    const rows = [];
 
-    const row = new ActionRowBuilder().addComponents(github);
+    //5 components and 5 rows
+    for (let i = 0; i < Math.floor((list.length + 4) / 5); ++i) {
+        const rowbuttons = buttons.slice(i * 5, i * 5 + 5);
+        const row = new ActionRowBuilder().addComponents(...rowbuttons);
+        rows.push(row);
+    }
 
-    interaction.reply({ embeds: embeds, components: [row] });
+    interaction.reply({ components: rows });
 }
 
 export const command_string = new SlashCommandBuilder()
