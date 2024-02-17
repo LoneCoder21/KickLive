78669aa765fce19cd9349ae3b989e6986cb1848d
Abhinav Neelam <abhinavneelam949@gmail.com>
2024-02-17 00:38:55 -0800

    add respective ui to reply once websocket is received for live event

--- src/pusher.ts
+++ src/pusher.ts
@@ -3,7 +3,10 @@ import axios from "axios";
 import { HEADERS } from "./constants/headers.js";
 import { Livestream } from "./Livestream.js";
 import { getDiscordClient } from "./discord.js";
-import { API_V2_URL } from "./constants/url.js";
+import { ButtonStyle, ButtonBuilder, EmbedBuilder, ActionRowBuilder } from "discord.js";
+import { API_V2_URL, STREAMER_URL, PROFILE_PIC_URL, GITHUB_URL } from "./constants/url.js";
+import { getDatabase } from "./db/db.js";
+import { User } from "./types/User.js";
 
 const watchstreamers = new Set<string>();
 const pusher = new Pusher("eb1d5f283081a78b932c", {
@@ -17,23 +20,64 @@ export async function subscribePusher(name: string) {
     const res = await axios.get(API_V2_URL(name), {
         headers: HEADERS
     });
-    console.log(res.data);
-    const id = res.data.id;
-    const followers_count = res.data.followers_count;
-    //const thumbnail = res.data.livestream.thumbnail.url;
-    //const title = res.data.livestream.session_title;
-    const categories = res.data.recent_categories;
+    const channelid = res.data.id;
 
-    const channel = pusher.subscribe(`channel.${id}`);
+    const channel = pusher.subscribe(`channel.${channelid}`);
     pusher.connection.bind("error", function (err) {
         console.log(err);
     });
-    channel.bind("App\\Events\\StreamerIsLive", (data: Livestream) => {
+    channel.bind("App\\Events\\StreamerIsLive", async (data: Livestream) => {
+        console.log(data);
         const client = getDiscordClient();
-        const discord_channel = client.channels.cache.get();
-
         if (!client.isReady) return;
 
-        discord_channel.send(`${name} - live right now at https://kick.com/${name} - ${data.session_title}`);
+        const res = await axios.get(API_V2_URL(name), {
+            headers: HEADERS
+        });
+        console.log(res.data);
+        const user: User = res.data.user;
+        const followers_count = res.data.followers_count;
+        const thumbnail = res.data.livestream.thumbnail.url;
+        const categories = res.data.recent_categories;
+
+        const table = await getDatabase();
+
+        const list = await table.findAll({
+            where: {
+                streamer: name
+            }
+        });
+
+        const embed = new EmbedBuilder()
+            .setColor(0x00ff7f)
+            .setAuthor({ name: name, iconURL: user.profile_pic, url: STREAMER_URL(name) })
+            .setTitle(data.session_title)
+            .setURL(STREAMER_URL(name))
+            .addFields({ name: "Category", value: categories[0].name })
+            .setImage(user.profile_pic)
+            .setFooter({
+                text: data.created_at,
+                iconURL: PROFILE_PIC_URL
+            });
+
+        const streambutton = new ButtonBuilder()
+            .setLabel("Watch Stream")
+            .setURL(STREAMER_URL(name))
+            .setStyle(ButtonStyle.Link);
+
+        const github = new ButtonBuilder().setLabel("GitHub").setURL(GITHUB_URL).setStyle(ButtonStyle.Link);
+        const row = new ActionRowBuilder().addComponents(streambutton, github);
+
+        const channelIDS = list.map((entry) => entry.channelID);
+        for (const channelID of channelIDS) {
+            const discord_channel = client.channels.cache.get(channelID);
+            discord_channel.send({
+                content: `@everyone ${name} is now live on Kick!`,
+                embeds: [embed],
+                components: [row]
+            });
+        }
     });
 }
+
+// TODO - Refactor discord UI code into separate components and remove duplication
