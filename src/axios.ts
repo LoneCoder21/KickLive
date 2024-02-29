f83fcf06c95d7d75094dbee56ed48a5738d7557e
Abhinav Neelam <abhinavneelam949@gmail.com>
2024-02-28 16:55:25 -0800

    Merge readme

--- src/axios.ts
+++ src/axios.ts
@@ -3,6 +3,15 @@ import { HEADERS } from "./constants/headers.js";
 
 export const GET = async (url: string) => {
     return axios.get(url, {
-        headers: HEADERS
+        headers: HEADERS,
+        proxy: {
+            protocol: "http",
+            host: "geo.iproyal.com",
+            port: 11250,
+            auth: {
+                username: "wKOq7OMRkolH1Eo1",
+                password: "CJ223ZvSYOCwiIag_country-us"
+            }
+        }
     });
 };
