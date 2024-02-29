654a52f68c8b2467c9b9d7e0aa36669ad31d0837
Abhinav Neelam <abhinavneelam949@gmail.com>
2024-02-28 16:56:33 -0800

    cleanup

--- src/axios.ts
+++ src/axios.ts
@@ -3,15 +3,6 @@ import { HEADERS } from "./constants/headers.js";
 
 export const GET = async (url: string) => {
     return axios.get(url, {
-        headers: HEADERS,
-        proxy: {
-            protocol: "http",
-            host: "geo.iproyal.com",
-            port: 11250,
-            auth: {
-                username: "wKOq7OMRkolH1Eo1",
-                password: "CJ223ZvSYOCwiIag_country-us"
-            }
-        }
+        headers: HEADERS
     });
 };
